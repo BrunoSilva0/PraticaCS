b95fb3bd298653cace1528b79c1c0070e3a41006
BrunoSilva0 <bruno.araujo@sptech.school>
2024-05-10 14:57:56 -0300

    Adição de um sistema de login (local) ao cliente

--- ConsoleApp/SistemaLoja/Cliente.cs
+++ ConsoleApp/SistemaLoja/Cliente.cs
@@ -1,17 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ConsoleApplication1.SistemaLoja
 {
     public class Cliente
     {
-        private string email;
-        private string nomeUsuario;
-        private string senha;
+        public string Email { get; set; }
+        public string Senha { get; set; }
 
-        public Cliente(string email, string nomeUsuario, string senha)
+        public Cliente(string email, string senha)
         {
-            this.email = email;
-            this.nomeUsuario = nomeUsuario;
-            this.senha = senha;
+            Email = email;
+            Senha = senha;
+        }
+    }
+
+    public class GerenciadorUsuarios
+    {
+        private List<Cliente> usuarios;
+        private string caminhoArquivo;
+
+        public GerenciadorUsuarios()
+        {
+            string caminhoPastaUsuario = Environment.GetEnvironmentVariable("USERPROFILE");
+            caminhoArquivo = Path.Combine(caminhoPastaUsuario, "usuarios.json");
+
+            if (File.Exists(caminhoArquivo))
+            {
+                var json = File.ReadAllText(caminhoArquivo);
+                usuarios = JsonConvert.DeserializeObject<List<Cliente>>(json);
+            }
+            else
+            {
+                usuarios = new List<Cliente>();
+            }
+        }
+
+        public void AdicionarUsuario(Cliente usuario)
+        {
+            usuarios.Add(usuario);
+            SalvarDados();
+        }
+
+        public bool ChecarLogin(string email, string senha)
+        {
+            foreach (var usuario in usuarios)
+            {
+                if (usuario.Email == email && usuario.Senha == senha)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void SalvarDados()
+        {
+            var json = JsonConvert.SerializeObject(usuarios);
+            File.WriteAllText(caminhoArquivo, json);
         }
     }
 }
--- ConsoleApp/SistemaLoja/MainProgram.cs
+++ ConsoleApp/SistemaLoja/MainProgram.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1.SistemaLoja
         {
             const string mensagemEntrada = @"
     _   ___                                       
-   / | / (_)_______     ____  ____ _____ ___  ___ 
+   / | / (_)_______     ____  ____  ____ ___  ___ 
   /  |/ / / ___/ _ \   / __ \/ __ `/ __ `__ \/ _ \
  / /|  / / /__/  __/  / / / / /_/ / / / / / /  __/
 /_/ |_/_/\___/\___/  /_/ /_/\__,_/_/ /_/ /_/\___/ ";
@@ -20,50 +20,117 @@ namespace ConsoleApplication1.SistemaLoja
             Thread.Sleep(2000);
             Console.Clear();
             Console.WriteLine("Escolha sua operação:");
-            const string operacao = @"1 - Operação 1
+            Console.WriteLine(@"1 - Login / Cadastro
 2 - Operação 2
 3 - Operação 3
 4 - Operação 4
 5 - Operação 5
-";
-            
-            Console.WriteLine(operacao);
+");
             ConsoleKeyInfo tecla = Console.ReadKey();
             Console.WriteLine("\n");
             switch (tecla.KeyChar)
             {
                 case '1':
-                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
                     
-                    //beep beep
-                    Console.Beep(); 
-                    Thread.Sleep(100);
                     Console.Beep();
-                    
                     Console.Write("Operação 1 foi escolhida");
                     Console.ResetColor();
+
+                    Console.WriteLine("\nVocê já tem uma conta? [S / N]");
+                    string operacaoEscolhida = Console.ReadLine();
+                    // Login
+                    if (operacaoEscolhida == "S" || operacaoEscolhida == "Sim")
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("Entre suas credenciais");
+                        Console.ResetColor();
+                        
+                        Console.WriteLine("\nEmail:");
+                        string emailEntrada = Console.ReadLine();
+
+                        Console.WriteLine("Senha:");
+                        string senhaEntrada = Console.ReadLine();
+
+                        Cliente cliente = new Cliente(emailEntrada, senhaEntrada);
+                        
+                        GerenciadorUsuarios gerenciador = new GerenciadorUsuarios();
+                        bool existe = gerenciador.ChecarLogin(emailEntrada, senhaEntrada);
+                        if (existe)
+                        {
+                            Console.WriteLine("Login feito com êxito!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Conta não encontrada");
+                        }
+                    // Cadastro
+                    } else if (operacaoEscolhida == "N" || operacaoEscolhida == "Nao")
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("Faça o cadastro");
+                        Console.ResetColor();
+                        
+                        Console.WriteLine("\nEmail:");
+                        string emailEntrada = Console.ReadLine();
+
+                        Console.WriteLine("Senha:");
+                        string senhaEntrada = Console.ReadLine();
+
+                        Cliente cliente = new Cliente(emailEntrada, senhaEntrada);
+                        GerenciadorUsuarios gerenciadorUsuarios = new GerenciadorUsuarios();
+                        gerenciadorUsuarios.AdicionarUsuario(cliente);
+                        
+                        Console.WriteLine("Inscrição feita! :D");
+                        Console.WriteLine("Aviso: um novo arquivo foi criado no seu usuário atual, caso queria removê-lo, vá para pasta do seu usuário atual");
+                        Thread.Sleep(1500);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada não esperada");
+                        Console.WriteLine("Fechando...");
+                        Thread.Sleep(500);
+                        return;
+                    }
+                    
                     break;
                 case '2':
-                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    
+                    Console.Beep();
                     Console.Write("Operação 2 foi escolhida");
                     Console.ResetColor();
                     break;
                 case '3':
-                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    
+                    Console.Beep();
                     Console.Write("Operação 3 foi escolhida");
                     Console.ResetColor();
                     break;
                 case '4':
-                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    
+                    Console.Beep();
                     Console.Write("Operação 4 foi escolhida");
                     Console.ResetColor();
                     break;
                 case '5':
                     Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    
+                    Console.Beep();
                     Console.Write("Operação 5 foi escolhida");
                     Console.ResetColor();
                     break;
             }
+            Thread.Sleep(3000);
         }
     }
 }
